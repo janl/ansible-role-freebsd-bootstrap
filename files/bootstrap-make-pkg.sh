488bacaf409aba98b79295e70c986ae518b5e8ee
niladic <niladic@gmail.com>
2014-03-20 08:23:57 +0100

    Fix pkg was not installed after a removal with pkg delete.

--- files/bootstrap-make-pkg.sh
+++ files/bootstrap-make-pkg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -e /var/db/pkg/local.sqlite ]; then
+if [ ! -e /var/db/pkg/local.sqlite ] || ! `pkg -N`; then
   make -C /usr/ports/ports-mgmt/pkg -DBATCH install clean
 else
   echo "pkgng already installed."
